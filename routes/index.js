9d73f0461b16e18bc982c6fca693886b71f1f956
EmmzyTech <emmzytech@gmail.com>
2023-02-20 09:32:40 -0500

    Updated Routes

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,13 @@ const {
 } = require("../controller")
 
 
-Router.get("/", home)
+Router.get("/", home);
 
 Router.post("/addadmin", addAdmin);
 Router.post("/admin/login", login);
 Router.post("/create/tribute", addTribute);
-Router.post("/ticket/approve", approveTribute)
-Router.post("/ticket/delete", deleteTribute,)
+Router.post("/ticket/approve", approveTribute);
+Router.post("/ticket/delete", deleteTribute);
 
 
 
